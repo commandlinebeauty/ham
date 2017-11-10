29e230478a4b13e83d32a1346badf4efff2e2b6b
Fritz-Walter Schwarm <fritz.schwarm@sternwarte.uni-erlangen.de>
2017-11-10 12:59:42 +0100

    plain layout file

---
+++ plain.php
@@ -0,0 +1,43 @@
+<?php
+
+class hamLayoutPlain extends hamLayout
+{
+	public function __construct($buffer, $cfg = null)
+	{
+		//! Call general layout constructor
+		parent::__construct($buffer, $cfg);
+	}
+
+	//! Render HTML table and return as string
+	public function render($buffer, $cfg = null)
+	{
+		$tmp = clone $buffer;
+
+		foreach (parent::getBoxes() as $box) {
+
+			$content = $box->render($buffer, $cfg);
+
+			$overlay = new hamBuffer($content, $cfg);
+
+			$tmp->overlay(
+				//! Coordinates in buffer frame
+				$box->getRect(),
+				//! Overlay buffer and configuration
+				$overlay, $cfg
+			);
+		}
+
+		$out = "";
+
+		//! Render content as simple as possible
+		$out .= "<pre class='hamLayoutPlain'>\n";
+
+		$out .= ham_entities($tmp->getContent(), $cfg);
+	
+		$out .= "</pre>";
+	
+		return $out;
+	}
+}
+
+?>
